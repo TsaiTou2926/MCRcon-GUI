ed3a2e3549eeb237f28800f8828cf9e4bf7a706d
Tsaitou7361 <kyle.liu.0618@gmail.com>
2023-11-15 20:18:11 +0800

    update to 1.3!

--- os_detect.py
+++ os_detect.py
@@ -1,4 +1,5 @@
 import platform
+import sys
 
 def detect_sys(sys):
     if sys == "Linux":
@@ -11,13 +12,13 @@ def detect_sys(sys):
         os = "unknown"
     return os
 
-def detect_win_ver():
-    return platform.release()
+def is_win11():
+    return sys.getwindowsversion().build >= 22000
 
 if __name__ == "__main__":
     # example
     os = detect_sys(platform.system())
     print(f"Current OS: {os}")
     if os == "Windows":
-        win_ver = detect_win_ver()
-        print(f"Current Version: Windows {win_ver}")
+        win_ver = is_win11()
+        print(f"Windows 11: {win_ver}")
