d5704603922002d3ef1315760cfb04360c59d1a2
Tsaitou7361 <kyle.liu.0618@gmail.com>
2023-11-15 20:11:42 +0800

    update to 1.3!

--- os_detect.py
+++ os_detect.py
@@ -1,21 +1,23 @@
-import sys
+import platform
 
-def detect(sys):
-    if sys == "aix":
-        os = "aix"
-    elif sys == "linux":
+def detect_sys(sys):
+    if sys == "Linux":
         os = "Linux"
-    elif sys == "win32":
+    elif sys == "Windows":
         os = "Windows"
-    elif sys == "cygwin":
-        os = "Windows"
-    elif sys == "darwin":
+    elif sys == "Darwin":
         os = "MacOS"
     else:
         os = "unknown"
     return os
 
+def detect_win_ver():
+    return platform.release()
+
 if __name__ == "__main__":
     # example
-    os = detect(sys.platform)
+    os = detect_sys(platform.system())
     print(f"Current OS: {os}")
+    if os == "Windows":
+        win_ver = detect_win_ver()
+        print(f"Current Version: Windows {win_ver}")
